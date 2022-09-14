867caa414d360d2842b45bf6918b9328fd27771c
N1sgamm <N1sgamm@gmail.com>
2022-09-14 13:53:59 -0300

    Initial commit

---
+++ HelloWorld9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 9");
+            Console.WriteLine("*************");
+            Console.WriteLine("Zackery Goodwin");
+            Console.WriteLine("09/14/2022");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
